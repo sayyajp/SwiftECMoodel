d1973274573df6a4c2367cf5eea91fe8ea605d0f
sayyajp <say_yajp@yahoo.co.jp>
2019-02-07 14:23:06 +0900

    header added to top

---
+++ ec_model/ec_model/classes/Controller/Sub/TopHeaderView.swift
@@ -0,0 +1,29 @@
+//
+//  TopHeaderView.swift
+//  ec_model
+//
+//  Created by ゲスト on 2019/02/07.
+//  Copyright © 2019 ゲスト. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - Property
+class TopHeaderView: BaseView {
+    
+}
+
+// MARK: - Life cycle
+extension TopHeaderView {
+    
+}
+
+// MARK: - Protocol
+extension TopHeaderView {
+    
+}
+
+// MARK: - method
+extension TopHeaderView {
+    
+}
